dfad782e2868d053d21de8359323eece938668d1
Lucero <Lucero>
2017-03-29 14:01:00 -0600

    final

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,42 +1,53 @@
 function agregarPendiente(){
     var texto=document.getElementById('areaPendiente').value;
-    var li=document.createElement('LI');
+    var li=document.createElement('dt');
+    console.log(li);
   if( li.innerHTML=texto==''){
     (alert('No estas agregando un Pendiente?'))
   }else{
-    li.innerHTML=texto==''?'':texto;
+    li.innerHTML=texto==''?'':texto +"  <button onclick='borrarPendiente()'>X</button>";
     document.getElementById('Pendientes').appendChild(li);
     document.getElementById('areaPendiente').value ="";
+
   }
 };
-function borrarPendiente(){
-    lis=document.getElementById('Pendientes').getElementsByTagName('li');
-    for(var i=0; i<lis.length;i++){
-        lis[i].onclick=function(){
-            if(confirm('¿seguro que quieres eliminar esté Pendiente?'))
-            this.parentNode.removeChild(this);
-        };
-    }
-};
-//lista interna
 
+
+
+//funcion borrar elementos
+function borrarPendiente(){
+    lis=document.getElementById('Pendientes').getElementsByTagName('dt');
+        for(var i=0; i<lis.length;i++){
+        lis[i].onclick=function(){
+            if(confirm('¿seguro que quieres eliminar esto'))
+            this.parentNode.removeChild(this);
+      };
+  }
+};
+
+//lista interna
+  var lis2;
 function agregarActividad(){
     var texto=document.getElementById('areaPendiente').value;
-    var li=document.createElement('LI');
-  if( li.innerHTML=texto==''){
+  lis2=document.createElement('dd');
+  console.log(lis2);
+  if( lis2.innerHTML=texto==''){
     (alert('No estas agregando un Actividad?'))
   }else{
-    li.innerHTML=texto==''?'':texto;
-    document.getElementById('Actividads').appendChild(li);
-    document.getElementById('areaActividad').value ="";
-  }
+    lis2.innerHTML=texto==''?'':texto;
+    document.getElementById('Pendientes').appendChild(lis2);
+    document.getElementById('areaPendiente').value ="";
+    }
 };
-function borrarActividad(){
-    lis=document.getElementById('Actividads').getElementsByTagName('li');
-    for(var i=0; i<lis.length;i++){
+
+
+function borrarPendiente1(){
+    lis=elemento.parentNode.getAttribute('id');
+        for(var i=0; i<lis.length;i++){
         lis[i].onclick=function(){
-            if(confirm('¿seguro que quieres eliminar esté Actividad?'))
+            if(confirm('¿seguro que quieres eliminar esto'))
             this.parentNode.removeChild(this);
         };
     }
 };
+//prueba
