9e48a0d4b0700467223b0f3119be891956ee0e73
Lucero <Lucero>
2017-03-30 10:47:54 -0600

    logreBorrarelHijo

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,10 +2,10 @@ function agregarPendiente(){
     var texto=document.getElementById('areaPendiente').value;
     var li=document.createElement('dt');
     console.log(li);
-  if( li.innerHTML=texto==''){
+  if(texto === ''){
     (alert('No estas agregando un Pendiente?'))
   }else{
-    li.innerHTML=texto==''?'':texto +"  <button onclick='borrarPendiente()'>X</button>";
+    li.innerHTML = texto ===''?'':texto +"  <button onclick='borrarPendiente(this)'>X</button>";
     document.getElementById('Pendientes').appendChild(li);
     document.getElementById('areaPendiente').value ="";
 
@@ -18,7 +18,7 @@ function agregarPendiente(){
 function borrarPendiente(){
     lis=document.getElementById('Pendientes').getElementsByTagName('dt');
         for(var i=0; i<lis.length;i++){
-        lis[i].onclick=function(){
+        lis[i]=function(){
             if(confirm('¿seguro que quieres eliminar esto'))
             this.parentNode.removeChild(this);
       };
@@ -34,20 +34,17 @@ function agregarActividad(){
   if( lis2.innerHTML=texto==''){
     (alert('No estas agregando un Actividad?'))
   }else{
-    lis2.innerHTML=texto==''?'':texto;
+    lis2.innerHTML=texto==''?'':texto +"  <button onclick='borrarPendiente1(this)'>X</button>";
     document.getElementById('Pendientes').appendChild(lis2);
     document.getElementById('areaPendiente').value ="";
     }
 };
 
 
-function borrarPendiente1(){
-    lis=elemento.parentNode.getAttribute('id');
-        for(var i=0; i<lis.length;i++){
-        lis[i].onclick=function(){
-            if(confirm('¿seguro que quieres eliminar esto'))
-            this.parentNode.removeChild(this);
+function borrarPendiente1(elemento){
+    lis=elemento.parentNode.getAttribute('dd');
+    if(confirm('¿seguro que quieres eliminar esto')){
+            elemento.parentNode.remove(this);
         };
-    }
-};
+    };
 //prueba
